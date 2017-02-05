7c34fe0340ad8add396a57bab0e429f3e2a1aee5
Rahul Lakhaney <rahul@codebrew.io>
2017-02-05 18:34:50 +0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,8 +7,8 @@
 require('dotenv').config();
 
 module.exports = {
-  consumer_key: 'vLikdnLjysYeyyaStRNX93PGe',
-  consumer_secret: 'Zb3aVcLnizcBy2wFMpbcoO2RuUFAXDN0NfauRVTou0VlgBRCv6',
-  access_token: '531061716-LR6nFqpragIFTKKgY3uuKYQNsWnfKebI1YJCTo2R',
-  access_token_secret: 'e8CDtarzqgbuOU3VrYVs37W117Ing028RaUx8Ml2GMtFc',
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: '',
 };
